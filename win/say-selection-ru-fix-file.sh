7ea28f58c9a5acf800564318e7328e4053d4861e
d9k <d9k@ya.ru>
2018-04-04 00:20:24 +0300

    say selection ru: ")" fix

--- win/say-selection-ru-fix-file.sh
+++ win/say-selection-ru-fix-file.sh
@@ -21,7 +21,7 @@ sed -i -e 's|!|! |g' $1
 
 # temporarily, till problems with creating rules for square brackets fixed
 sed -i -e 's|\[| ( |g' $1
-sed -i -e 's|\]| ) |g' $1
+sed -i -e 's|\]| )|g' $1
 
 # fix и т. д., -> и т.  д. , => error
 sed -i -e 's|\. , |., |g' $1
