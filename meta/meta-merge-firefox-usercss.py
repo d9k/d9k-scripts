ae6d239c6c2513af9ce0915816e12e64ea4a1eef
d9k <d9k@ya.ru>
2018-04-03 19:24:18 +0300

    meta-merge-firefox-usercss: userChrome.css edit

--- meta/meta-merge-firefox-usercss.py
+++ meta/meta-merge-firefox-usercss.py
@@ -11,7 +11,7 @@ profiles_mozilla_path = home + r"/.mozilla/firefox"
 profiles_ini_path = profiles_mozilla_path + r"/profiles.ini"
 backup_cfg_folder_path = home + r"/scripts/cfg/firefox"
 backup_cfg_path = backup_cfg_folder_path + r"/userContent.css"
-
+backup_cfg_chrome_path = backup_cfg_folder_path + r"/userChrome.css"
 #TODO список утилит + проверка через which + задание из параметра
 edit_tool = "sublime-text"
 merge_tool = "meld"
@@ -64,6 +64,11 @@ def main():
         help="open editor in background",
         action='store_true'
     )
+    parser.add_argument(
+        '--chrome', '-c',
+        help="edit userChrome.css, not userContent.css",
+        action='store_true'
+    )
     args = parser.parse_args()
     #pprint.pprint(args)
     if not os.path.isfile(profiles_ini_path):
@@ -79,6 +84,8 @@ def main():
         return
     user_css_folder_path = profile_path + "/chrome"
     user_css_path = user_css_folder_path + "/userContent.css"
+    user_chrome_path = user_css_folder_path + "/userChrome.css"
+
     bash("mkdir", "-p", user_css_folder_path)
     bash("touch", user_css_path)
     print("""
@@ -91,10 +98,17 @@ def main():
 
     print("User css path: " + user_css_path)
 
-    if args.edit:
-        bash(edit_tool, user_css_path, bg(args.background))
+    if args.chrome:
+      css_path = user_chrome_path
+      backup_path = backup_cfg_chrome_path
     else:
-        bash(merge_tool, user_css_path, backup_cfg_path, bg(args.background))
+      css_path = user_css_path
+      backup_path = backup_cfg_path
+
+    if args.edit:
+        bash(edit_tool, css_path, bg(args.background))
+    else:
+        bash(merge_tool, css_path, backup_path, bg(args.background))
 
 if __name__ == "__main__":
     main()
