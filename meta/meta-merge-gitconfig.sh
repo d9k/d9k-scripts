dae377c35133197cece2782147bcbd277a26dcd2
d9k <d9k@ya.ru>
2020-03-18 12:29:30 +0300

    gitconfig+

---
+++ meta/meta-merge-gitconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CFG_DIR=~
+CFG_BACKUP_DIR=~/scripts/cfg
+FILE_REL_PATH=.gitconfig
+
+merge-util "$CFG_DIR/${FILE_REL_PATH}" "${CFG_BACKUP_DIR}/${FILE_REL_PATH}"
+
+echo "After merging data to \"${CFG_BACKUP_DIR}\":"
+echo
+cd "${CFG_BACKUP_DIR}"
+hg status .
