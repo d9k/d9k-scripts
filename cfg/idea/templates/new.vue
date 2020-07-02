975aa1dc7538a5c41cf830bcdfac8a02cd9911f7
d9k <d9k@ya.ru>
2020-07-02 21:57:57 +0300

    idea: +vue template

---
+++ cfg/idea/templates/new.vue
@@ -0,0 +1,15 @@
+<template>
+  #[[$END$]]#
+</template>
+
+<script lang="ts">
+export default {
+  name: "${COMPONENT_NAME}"
+  components: {},
+  props: {},
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style lang="sass" src="./styles.sass" />
