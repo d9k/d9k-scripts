7a4e93033392f7942de7a63c6d9c37a1121eeb27
d9k <d9k@ya.ru>
2021-12-25 23:07:40 +0300

    apt-ls-all-* scripts

---
+++ apt-ls-all-from-ppa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#https://askubuntu.com/questions/447129/how-to-list-all-the-packages-which-are-installed-from-ppas
+
+function unsorted_result {
+  for APT in $(find /etc/apt/ -name \*.list); do
+    grep -o "^deb http://ppa.launchpad.net/[a-z0-9\-]\+/[a-z0-9\-]\+" $APT | while read ENTRY ; do
+      USER=$(echo $ENTRY | cut -d/ -f4)
+      PPA=$(echo $ENTRY | cut -d/ -f5)
+      awk '$1 == "Package:" { if (a[$2]++ == 0) print $2; }' /var/lib/apt/lists/*$USER*$PPA*Packages
+      done
+  done
+}
+
+printf "$(unsorted_result)" | sort
