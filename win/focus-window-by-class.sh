460e4aeef344f3fe82f12b80737893a314706819
d9k <d9k@ya.ru>
2022-03-03 23:23:48 +0300

    focus-window-by-class: + --number arg

--- win/focus-window-by-class.sh
+++ win/focus-window-by-class.sh
@@ -1,22 +1,57 @@
 #!/bin/bash
 
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+help_exit() {
+  echo "Help: ${SCRIPT_NAME} [-n|--number WINDOW_NUMBER] WINDOW_CLASS [COMMAND]"
+  exit
+}
+
+WINDOW_NUMBER=1
+
+while [[ $# > 0 ]]; do
+    OPTION="$1"
+
+    case "$OPTION" in
+        -n|--number)
+            WINDOW_NUMBER="$2"
+            shift # past argument
+        ;;
+        -h|--help)
+            help_exit
+        ;;
+        *)
+          if [[ "${OPTION:0:1}" == "-" ]]; then
+            echo -e "Error: unknown option \"$OPTION\" \n"
+            help_exit
+          else
+            break
+          fi
+        ;;
+    esac
+    shift # past argument or value
+done
+
 WINDOW_CLASS="$1"
 #WINDOW_CLASS=Terminal
 COMMAND="$2"
 
 if [[ -z "$WINDOW_CLASS" ]]; then
   echo "WINDOW_CLASS not set"
+  help_exit
 fi
 
 # -x: + WIN CLASS
-WMCTRL_SEARCH_OUTPUT=$(wmctrl -lx | grep ${WINDOW_CLASS} | sed -n 1p)
+WMCTRL_SEARCH_OUTPUT=$(wmctrl -lx | grep ${WINDOW_CLASS} | sed -n ${WINDOW_NUMBER}p)
 
 echo $WMCTRL_SEARCH_OUTPUT
 
 WMCTRL_NUMBER=$(echo "$WMCTRL_SEARCH_OUTPUT" | awk '{print $1;}')
 
 if [[ -z "$WMCTRL_NUMBER" ]]; then
-  eval $COMMAND
+  if [[ -n "$COMMAND" ]]; then
+    eval $COMMAND
+  fi
   exit
 fi
 
