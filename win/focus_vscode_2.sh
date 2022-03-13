5115c46f1130656b94ec9ade98ba7bdf18e7c3e9
d9k <d9k@ya.ru>
2022-03-13 14:48:08 +0300

    focus_vscode_2

--- win/focus_vscode_2.sh
+++ win/focus_vscode_2.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 WINDOW_CLASS="code\.Code"
-WINDOW_NUMBER=1
+WINDOW_NUMBER=2
 
 ${SCRIPT_DIR}/focus-window-by-class.sh --number ${WINDOW_NUMBER} "$WINDOW_CLASS"
