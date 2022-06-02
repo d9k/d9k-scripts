2f9ca33f8c668b97f8b4d183d3322515c53342ec
d9k <d9k@ya.ru>
2022-06-02 13:20:45 +0300

    meta-merge-vscode-config

---
+++ meta/meta-merge-vscode-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CFG_DIR=~
+CFG_BACKUP_DIR=~/scripts/cfg/vscode
+FOLDER_REL_PATH=.config/Code/User
+
+mkdir -p "${CFG_BACKUP_DIR}"
+
+meld "$CFG_DIR/${FOLDER_REL_PATH}" "${CFG_BACKUP_DIR}"
+
+echo "After merging data to \"${CFG_BACKUP_DIR}\":"
+echo
+cd "${CFG_BACKUP_DIR}"
+git diff "${CFG_BACKUP_DIR}/${FOLDER_REL_PATH}"
+echo "${CFG_BACKUP_DIR}:"
+git status .
