a96729e9c1afc74ebd7a1d780994c802d0a2514f
d9k <d9k@ya.ru>
2022-08-22 22:33:05 +0300

    focus-vs-code-num

---
+++ win/focus-vscode-num.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+WINDOW_NUMBER="$1"
+
+WINDOW_CLASS="code\.Code"
+
+if [[ -z "$WINDOW_NUMBER" ]]; then
+  WINDOW_NUMBER=1
+fi
+
+function echoerr {                                                                      
+  printf "%s\n" "$*" >&2;             
+}
+
+WMCTRL_SEARCH_OUTPUT=$(\
+  wmctrl -lx | \
+  grep ${WINDOW_CLASS} | \
+  # исключения \
+  grep -v "diary (Workspace)" | \
+  grep -v "textbook (Workspace)" | \
+  sed -n ${WINDOW_NUMBER}p \
+)
+
+WMCTRL_NUMBER=$(echo "$WMCTRL_SEARCH_OUTPUT" | awk '{print $1;}')
+
+if [[ -z "$WMCTRL_NUMBER" ]]; then
+  echoerr "Windows with class \"$WINDOW_CLASS\" not found"
+  exit
+fi
+
+bash -c "$SCRIPT_DIR/focus-window-by-id.sh $WMCTRL_NUMBER"
--- win/focus_vscode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-WINDOW_CLASS="code\.Code"
-WINDOW_NUMBER=1
-
-${SCRIPT_DIR}/focus-window-by-class.sh --number ${WINDOW_NUMBER} "$WINDOW_CLASS"
