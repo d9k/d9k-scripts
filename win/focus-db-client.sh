1d5d1ee75722017a0fae1ad6eb8bcccce9181e26
d9k <d9k@ya.ru>
2022-08-23 08:36:43 +0300

    focus-db-client: autostart dbeaver

--- win/focus-db-client.sh
+++ win/focus-db-client.sh
@@ -12,12 +12,15 @@ for (( ix=${#titles[@]}-1 ; ix>=0 ; ix-- )); do
 	t=$(wmctrl -lp | grep "${titles[ix]}")
 	if [ $? -eq 1 ]; then
 		if [ "${ix}" -eq "0" ]; then
-			echo "Last one: starting HeidiSQL"
+			echo "Last one: starting db client"
+
+			dbeaver &
+
     	    #env WINEPREFIX="/home/d9k/.wine" wine C:\\windows\\command\\start.exe /Unix /home/d9k/.wine/dosdevices/c:/users/Public/Start\ Menu/Programs/HeidiSQL/HeidiSQL.lnk -d local
 
-          env WINEPREFIX="/home/d9k/.wine" wine C:\\windows\\command\\start.exe /Unix /home/d9k/.wine/dosdevices/c:/ProgramData/Microsoft/Windows/Start\ Menu/Programs/HeidiSQL/HeidiSQL.lnk
+			#env WINEPREFIX="/home/d9k/.wine" wine #C:\\windows\\command\\start.exe /Unix /home/d9k/.wine/dosdevices/c:/ProgramData/Microsoft/Windows/Start\ Menu/Programs/HeidiSQL/HeidiSQL.lnk
 
-          #env WINEPREFIX="/home/d9k/.wine" wine start /ProgIDOpen SQLScriptFile %f
+			#env WINEPREFIX="/home/d9k/.wine" wine start /ProgIDOpen SQLScriptFile %f
 	fi
 	else
 		echo "focusing \"${t}\""
