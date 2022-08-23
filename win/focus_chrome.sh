13407149b65e58a2301f32777efa75cb1019b7e0
d9k <d9k@ya.ru>
2022-08-23 08:34:49 +0300

    focus_chrome: forced fix suspend, +dark mode

--- win/focus_chrome.sh
+++ win/focus_chrome.sh
@@ -4,7 +4,8 @@ appTitle="Google Chrome"
 
 t=$(wmctrl -lp | grep "${appTitle}")
 if [ $? -eq 1 ]; then
-    google-chrome &
+    #google-chrome &
+    bash $HOME/scripts/chrome-fix-suspend &
 else
     wmctrl -a "${appTitle}"
 fi
