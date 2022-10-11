40b94d5f20d1f2c94e64cd60f4dec5f1d5f64c40
d9k <d9k@ya.ru>
2022-10-11 18:16:07 +0300

    compress-screenshot: mozjpeg instead of cwebp

--- compress-screenshot.sh
+++ compress-screenshot.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SCRIPT_DIR/is"
 
 MAX_ORIGINAL_ALLOWED_SIZE_KB=500
+QUALITY=96
 
 function echoerr {
   printf "%s\n" "$*" >&2;
@@ -42,28 +43,34 @@ fi
 
 FILE_SIZE_KB=$(file_size_kb "$FILE_ABS_PATH")
 
-echo $FILE_SIZE_KB
-
 if [ "$FILE_SIZE_KB" -le "$MAX_ORIGINAL_ALLOWED_SIZE_KB"  ]; then
   echo "Skipping compression: file size is just ${FILE_SIZE_KB}kb (<= ${MAX_ORIGINAL_ALLOWED_SIZE_KB}kb)"
   exit
 fi
 
-if ! command -v cwebp &> /dev/null ;then
-    echoerr "cwebp command could not be found. Install webp package first"
+# if ! command -v cwebp &> /dev/null ;then
+#     echoerr "cwebp command could not be found. Install webp package first"
+#     exit 30
+# fi
+
+if ! command -v mozjpeg &> /dev/null ;then
+    echoerr "mozjpeg command could not be found. Do \"npm install --global mozjpeg\" first"
     exit 30
 fi
 
-OUTPUT_PATH="$FILE_DIR_PATH/$FILE_NAME_NO_EXT.webp"
+OUTPUT_PATH="$FILE_DIR_PATH/$FILE_NAME_NO_EXT.jpg"
 
-cwebp -q 100 "$FILE_ABS_PATH" -o "$OUTPUT_PATH"
+# cwebp -q 100 "$FILE_ABS_PATH" -o "$OUTPUT_PATH" &> /dev/null
+
+mozjpeg -quality "$QUALITY" -outfile "$OUTPUT_PATH" "$FILE_ABS_PATH"
 
 OUTPUT_FILE_SIZE_KB=$(file_size_kb "$OUTPUT_PATH")
 
 if [ "$OUTPUT_FILE_SIZE_KB" -lt "$FILE_SIZE_KB" ]; then
   PERCENT_DELTA_RAW=$(echo "($FILE_SIZE_KB-$OUTPUT_FILE_SIZE_KB)/$FILE_SIZE_KB*100" | bc -l)
   PERCENT_DELTA=$(printf %.0f "$PERCENT_DELTA_RAW")
-  echo "compression png -> webp: (-${PERCENT_DELTA}%: ${FILE_SIZE_KB}kb -> ${OUTPUT_FILE_SIZE_KB}kb)"
+#   echo "compression png -> webp: (-${PERCENT_DELTA}%: ${FILE_SIZE_KB}kb -> ${OUTPUT_FILE_SIZE_KB}kb)"
+  echo "compression png -> jpg: (-${PERCENT_DELTA}%: ${FILE_SIZE_KB}kb -> ${OUTPUT_FILE_SIZE_KB}kb)"
   rm "$FILE_ABS_PATH" &> /dev/null
 else
   echo "Compression was not useful. Deleting output file"
