1885782991915bf6d84ab7a92a1ea8df156db317
d9k <d9k@ya.ru>
2022-10-11 19:45:13 +0300

    take_screenshot_optimized.sh

---
+++ take_screenshot_optimized.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Or , instead of . as fraction part separator if called from shortcut, WTF?!
+export LC_ALL=en_US.UTF-8
+
+# scrot --focused '%Y_%m_%d__%H_%M_%S__$wx$h.png' -e 'mv $f ~/screenshots/'
+scrot \
+    --focused '%Y_%m_%d__%H_%M_%S__$wx$h.png' \
+    -e 'mv $f ~/screenshots/'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm use default > /dev/null
+
+bash "$SCRIPT_DIR/compress-screenshots.sh" ~/screenshots
