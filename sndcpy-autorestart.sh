f5f0c055619bc75ff28570aace8b40e18cfcb94e
d9k <d9k@ya.ru>
2023-02-03 19:55:53 +0300

    sndcpy-autorestart: fix CPU overheat

---
+++ sndcpy-autorestart.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script to restart sndcpy to minimize delay accumulation
+#
+# sndcpy - Forward android audio to pc:
+# https://github.com/rom1v/sndcpy
+
+# crontab:
+# */15 * * * * /bin/bash /home/d9k/scripts/sndcpy-autorestart.sh
+
+# Or no sound!
+export XDG_RUNTIME_DIR="/run/user/1000"
+
+PID=$(pgrep -f '\.\/sndcpy$')
+
+if [ -z "$PID" ]; then
+  echo not found sndcpy;
+  exit
+fi
+
+kill -9 "$PID"
+killall vlc
+
+sleep 1
+
+cd /home/d9k/soft/sndcopy
+#/usr/bin/bash ./sndcpy >> /var/log/sndcpy.log &
+/usr/bin/bash ./sndcpy >> /var/log/sndcpy.log 2>&1 &
