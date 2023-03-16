ff159bf8aaaf2c2973f153e47acc7ed597d02f65
d9k <d9k@ya.ru>
2023-03-17 01:54:00 +0300

    win: focus_webstorm_third

--- win/focus_webstorm_third.sh
+++ win/focus_webstorm_third.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 
-#lower => better priority
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Закомментировал, т. к. в некоторых случаях неподходящий под шаблон класс jetbrains-phpstorm-debug
-#WINDOW_CLASS=jetbrains-phpstorm.jetbrains-phpstorm
-#WINDOW_CLASS=jetbrains-phpstorm
-WINDOW_CLASS=jetbrains-webstorm
+WINDOW_CLASS_WEBSTORM=jetbrains-webstorm
+WINDOW_CLASS_IDEA=jetbrains-idea
 
 WINDOW_NUMBER=3
 
-# selecting second window
+${SCRIPT_DIR}/focus-window-by-class.sh --number "$WINDOW_NUMBER" "$WINDOW_CLASS_WEBSTORM"
 
-# -x: + WIN CLASS
-WMCTRL_SEARCH_OUTPUT=$(wmctrl -lx | grep ${WINDOW_CLASS} | sed -n ${WINDOW_NUMBER}p)
-
-echo $WMCTRL_SEARCH_OUTPUT
-
-WMCTRL_NUMBER=$(echo "$WMCTRL_SEARCH_OUTPUT" | awk '{print $1;}')
-
-if [[ -z "$WMCTRL_NUMBER" ]]; then
-  exit
+if [[ "$?" != "0" ]]; then
+  ${SCRIPT_DIR}/focus-window-by-class.sh --number "$WINDOW_NUMBER" "$WINDOW_CLASS_IDEA"
 fi
-
-# echo ${WMCTRL_NUMBER}
-
-set -x
-# -v: verbose
-# -i: int value, not caption text
-# -a: activate
-wmctrl -v -i -a  ${WMCTRL_NUMBER}
-
-#if [ $? -eq 1 ]; then
-#  if [ "${ix}" -eq "0" ]; then
-#    echo 1
-#    #echo "Last one: starting NetBeans"
-#    #/bin/bash -c "source /home/d9k/.rvm/scripts/rvm; /home/d9k/netbeans-8.0/bin/netbeans" &
-#  fi
-#else
-#  echo "focusing \"${t}\""
-#  wmctrl -a "${titles[ix]}"
-#  break
-#  fi
