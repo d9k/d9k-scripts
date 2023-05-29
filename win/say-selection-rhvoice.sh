3a3fe996d23537e6a0df1674649d8a4cafa9f7ef
d9k <d9k@ya.ru>
2023-05-30 04:10:10 +0500

    say-selection-rhvoice: sync

--- win/say-selection-rhvoice.sh
+++ win/say-selection-rhvoice.sh
@@ -8,10 +8,24 @@
 SCRIPTS="$HOME/scripts"
 SCRIPT_RHVOICE_FIXED="$SCRIPTS/rhvoice-fixed.sh"
 
-killall RHVoice-client && sleep 3
+killall RHVoice-client
+killall RHVoice-service
+sleep 1
+RHVoice-service &
+sleep 1
+
+
 
 #xsel | RHVoice-client -r 0.5 -v 0.8 -p 0.05 -s Aleksandr+Alan | aplay
 
-xsel | "$SCRIPT_RHVOICE_FIXED" | aplay
+# TODO пропадает часть слов в начала текста: временно сохраняю в промежуточный файл.
+#   Но вообще с запущенным pavucontrol не дожно быть проблем
 
+#xsel | "$SCRIPT_RHVOICE_FIXED" | aplay
+xsel | "$SCRIPT_RHVOICE_FIXED" > /tmp/t.wav
+sleep 1
+aplay /tmp/t.wav
+sleep 1
+killall RHVoice-service
 #xsel | RHVoice-client -r 0.5 -v 0.8 -p 0.05 -s Irina+Alan | aplay
+
