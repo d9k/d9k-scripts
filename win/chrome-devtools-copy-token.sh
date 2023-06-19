39db8ae127050093fe0cf83983c64f7694d12b73
d9k <d9k@ya.ru>
2023-06-19 17:42:57 +0500

    chrome-devtools-copy-token

--- win/chrome-devtools-copy-token.sh
+++ win/chrome-devtools-copy-token.sh
@@ -5,7 +5,7 @@ xdotool sleep 0.4 key --clearmodifiers Down
 xdotool sleep 0.1 key --repeat 3 --delay 0.03 --clearmodifiers Down
 xdotool sleep 0.1 key --clearmodifiers Return
 xdotool sleep 0.2 key --repeat 8 --delay 0.03 --clearmodifiers Down
-xdotool sleep 0.1 key --clearmodifiers Return
+xdotool sleep 0.5 key --clearmodifiers Return
 
 CLIPBOARD=$(xsel -ob)
 # AUTH_TOKEN=$(grep "\-H 'Authorization:" | awk -v FS="('Authorization: Bearer |')" '{print $2}')
