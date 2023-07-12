6abf48e800c0676ae4118ae944460e03424aeca4
d9k <d9k@ya.ru>
2023-07-12 23:19:48 +0500

    focus-obsidian: get rid of flatpak

--- win/focus-obsidian.sh
+++ win/focus-obsidian.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 
 WINDOW_CLASS="obsidian.obsidian"
 
-COMMAND="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=obsidian.sh --file-forwarding md.obsidian.Obsidian"
+#COMMAND="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=obsidian.sh --file-forwarding md.obsidian.Obsidian"
+COMMAND="obsidian"
 
 bash -c "$SCRIPT_DIR/focus-window-by-class.sh \"$WINDOW_CLASS\" \"$COMMAND\""
