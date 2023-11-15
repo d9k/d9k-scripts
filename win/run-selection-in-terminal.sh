608898b10e193c79e11861c4be403bd524360b28
d9k <d9k@ya.ru>
2023-11-15 05:17:31 +0500

    run-selection-in-terminal: Run code between back quotes only, if present (for markdown) ``

--- win/run-selection-in-terminal.sh
+++ win/run-selection-in-terminal.sh
@@ -8,6 +8,13 @@ xdotool sleep 0.2 key --clearmodifiers "Control_L+c"
 
 ${SCRIPT_DIR}/focus_terminal.sh
 
+CLIPBOARD=$(xsel -ob)
+
+# Run code between back quotes only, if present (for markdown) ``
+CLIPBOARD_FIXED=$(echo -e "$CLIPBOARD" |  cut -d "\`" -f2)
+
+echo -e "$CLIPBOARD_FIXED" | xclip -selection c
+
 xdotool sleep 1.5 key --clearmodifiers "Control_L+Shift+v"
 xdotool sleep 0.4 key --clearmodifiers "Return"
 xdotool sleep 0.4 key --clearmodifiers "Return"
