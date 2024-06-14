81ee6390c1b14666420a6b18ab00f8bc08983580
d9k <d9k@ya.ru>
2024-06-14 19:27:46 +0500

    focus-obsidian: default vault

--- win/focus-obsidian.sh
+++ win/focus-obsidian.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 WINDOW_CLASS="obsidian.obsidian"
 
 #COMMAND="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=obsidian.sh --file-forwarding md.obsidian.Obsidian"
-COMMAND="obsidian"
+# COMMAND="obsidian"
+COMMAND="obsidian obsidian://open/?vault=tx"
 
 bash -c "$SCRIPT_DIR/focus-window-by-class.sh \"$WINDOW_CLASS\" \"$COMMAND\""
