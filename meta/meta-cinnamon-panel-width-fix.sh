0495bdc95c6cc9f53fb90ab537841ba6cbfc02cb
d9k <d9k@ya.ru>
2024-06-24 06:00:21 +0500

    meta-cinnamon-panel-width-fix

---
+++ meta/meta-cinnamon-panel-width-fix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gsettings set org.cinnamon panels-height "['1:100']"
+gsettings set org.cinnamon panel-zone-text-sizes "[{\"left\": 11.5, \"center\": 12.0, \"right\": 42.0, \"panelId\": 1}]"
