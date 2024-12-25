e932afe914dc290c8d70fad15d3a2e50a155897e
d9k <d9k@ya.ru>
2024-12-25 07:43:02 +0500

    wmctrl-list-reorder: ~/.wmctrl-list-first-hex-ids instead of ~/.wmctrl-list-first-regex

--- win/wmctrl-list-reorder.sh
+++ win/wmctrl-list-reorder.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Content example: 
-# 0x08600004\|0x08400006
-WMCTRL_CFG_ORDER="$HOME/.wmctrl-list-first-regex"
+# 0x08600004
+# 0x08400006
+WMCTRL_CFG_ORDER="$HOME/.wmctrl-list-first-hex-ids"
 
 if [ ! -f "$WMCTRL_CFG_ORDER" ]; then
   wmctrl -l "$@" 
   exit
 fi
 
-CFG_ORDER=$(cat "$WMCTRL_CFG_ORDER")
+WMCTRL_CFG_ORDER_CONTENT=$(cat "$WMCTRL_CFG_ORDER")
 
-if [[ -z "$CFG_ORDER" ]]; then
+if [[ -z "$WMCTRL_CFG_ORDER_CONTENT" ]]; then
   wmctrl -l "$@" 
   exit
 fi
 
-RESULT_BEGIN=$(wmctrl -l "$@" | grep "$CFG_ORDER")
-RESULT_END=$(wmctrl -l "$@" | grep -v "$CFG_ORDER")
+ORDER_REGEX=$(echo -e "$WMCTRL_CFG_ORDER_CONTENT" | awk 'ORS="\\|"' 2>/dev/null | head -c -2)
+
+RESULT_BEGIN=$(wmctrl -l "$@" | grep "$ORDER_REGEX")
+RESULT_END=$(wmctrl -l "$@" | grep -v "$ORDER_REGEX")
 
 echo -e "$RESULT_BEGIN"
 echo -e "$RESULT_END"
