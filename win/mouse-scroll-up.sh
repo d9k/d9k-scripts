b28ad80587e288cfbe09554a4c1ec5b9a4e87b91
d9k <d9k@ya.ru>
2025-04-01 06:31:40 +0500

    mouse-scroll-{up/down}: accept lines count argument

--- win/mouse-scroll-up.sh
+++ win/mouse-scroll-up.sh
@@ -4,5 +4,13 @@
 #xdotool keyup Super
 #sleep 0.1
 
-xdotool click 4
+SCROLL_COUNT=$1
+
+if [[ -z "$SCROLL_COUNT" ]]; then
+  SCROLL_COUNT=1
+fi
+
+for run in {1..SCROLL_COUNT}; do
+  xdotool click 4
+done
 
