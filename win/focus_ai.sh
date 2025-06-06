fc119664c058411c4f1ef56e051177b3a1eccbdd
d9k <d9k@ya.ru>
2025-06-06 07:07:15 +0500

    win/focus_ai

---
+++ win/focus_ai.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#lower => better priority
+
+# Закомментировал, т. к. в некоторых случаях неподходящий под шаблон класс jetbrains-phpstorm-debug
+#WINDOW_CLASS=jetbrains-phpstorm.jetbrains-phpstorm
+WINDOW_CLASS="lm studio.LM Studio"
+
+COMMAND="echo Autorun not implemented! Run manually!"
+
+# WINDOW_NUMBER=1
+
+# selecting second window
+
+# -x: + WIN CLASS
+# WMCTRL_SEARCH_OUTPUT=$(wmctrl -lx | grep ${WINDOW_CLASS_PATTERN} | sed -n ${WINDOW_NUMBER}p)
+
+# echo "Found: $WMCTRL_SEARCH_OUTPUT"
+
+# WMCTRL_NUMBER=$(echo "$WMCTRL_SEARCH_OUTPUT" | awk '{print $1;}')
+
+# if [[ -z "$WMCTRL_NUMBER" ]]; then
+
+#   if [[ -z "$APP" ]]; then
+#     echo "No app defined. Exitting"
+#     exit
+#   else
+#     echo "Running app $APP"
+#     $APP &
+#   fi
+
+
+# else
+#   # echo ${WMCTRL_NUMBER}
+
+#   set -x
+#   # -v: verbose
+#   # -i: int value, not caption text
+#   # -a: activate
+#   wmctrl -v -i -a  ${WMCTRL_NUMBER}
+# fi
+
+
+
+#if [ $? -eq 1 ]; then
+#  if [ "${ix}" -eq "0" ]; then
+#    echo 1
+#    #echo "Last one: starting NetBeans"
+#    #/bin/bash -c "source /home/d9k/.rvm/scripts/rvm; /home/d9k/netbeans-8.0/bin/netbeans" &
+#  fi
+#else
+#  echo "focusing \"${t}\""
+#  wmctrl -a "${titles[ix]}"
+#  break
+#  fi
+
+${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
