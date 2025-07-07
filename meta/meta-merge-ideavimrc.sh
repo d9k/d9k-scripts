7c9b2168530ea321c169a97520208550f66af4e0
d9k <d9k@ya.ru>
2025-07-07 12:15:47 +0500

    + cfg/.ideavimrc

---
+++ meta/meta-merge-ideavimrc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_CFG_DIR=~
+BACKUP_CFG_DIR=~/scripts/cfg
+CFG_FILE_REL_PATH=.ideavimrc
+
+SOURCE_FILE_FULL_PATH="${SOURCE_CFG_DIR}/${CFG_FILE_REL_PATH}"
+BACKUP_FILE_FULL_PATH="${BACKUP_CFG_DIR}/${CFG_FILE_REL_PATH}"
+
+merge-util "${SOURCE_FILE_FULL_PATH}" "${BACKUP_FILE_FULL_PATH}"
+
+echo "After merging data to \"${BACKUP_FILE_FULL_PATH}\":"
+echo
+cd "${BACKUP_CFG_DIR}"
+echo
+git diff "${BACKUP_FILE_FULL_PATH}"
+git status .
+echo "You can:"
+echo "\`cd ${BACKUP_CFG_DIR}\`"
