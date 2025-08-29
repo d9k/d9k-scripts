72dc3b4e6da377050007109fd19cf1be369aaef9
d9k <d9k@ya.ru>
2025-08-29 23:22:05 +0500

    win/: border-add, border-remove: support window id parameter

--- win/border-remove.sh
+++ win/border-remove.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-win_id=$(xdotool getactivewindow)
-echo $win_id
+win_id="$1"
+
+if [[ -z "$win_id" ]]; then
+  win_id=$(xdotool getactivewindow)
+  echo $win_id
+fi
+
 xprop -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0" -id ${win_id}
