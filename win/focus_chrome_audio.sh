cb45f67dfdb1d6b5dfb1c54667db1a1c0d10f544
d9k <d9k@ya.ru>
2025-09-03 13:53:20 +0500

    win/focus_chrome_audio.sh, win/focus_chrome_proxy.sh

---
+++ win/focus_chrome_audio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+WINDOW_CLASS="Chrome Audio"
+
+COMMAND="chrome-audio-proxy"
+
+${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
