e825493bc2ae5c73afdc36058139b3d6f8131e95
d9k <d9k@ya.ru>
2025-10-11 16:08:06 +0500

    focus draw app, obs, vnyan windows scripts

---
+++ win/focus_draw_app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Посмотреть список классов открытых окон:
+# wmctrl -lx
+
+# (Чем выше, тем приоритетнее)
+WINDOW_CLASSES=(
+  "gimp.Gimp"
+)
+
+for (( ix=0 ; ix<${#WINDOW_CLASSES[@]} ; ix++ )); do
+  WINDOW_CLASS="${WINDOW_CLASSES[ix]}"
+	echo "checking \"${WINDOW_CLASS}\":"
+  WINDOW_NUMBER=1
+
+  # wmctrl: -x: output WIN CLASS too
+  WMCTRL_SEARCH_OUTPUT=$(wmctrl -lx | grep ${WINDOW_CLASS} | sed -n ${WINDOW_NUMBER}p)
+
+  echo $WMCTRL_SEARCH_OUTPUT
+
+  WMCTRL_NUMBER=$(echo "$WMCTRL_SEARCH_OUTPUT" | awk '{print $1;}')
+
+  if [[ -n "$WMCTRL_NUMBER" ]]; then
+    echo "Focusing ${WMCTRL_NUMBER}"
+
+    # wmctrl:
+    # -a: activate
+    # -i: int value, not caption text
+    # -v: verbose
+    ( set -x; wmctrl -v -i -a  ${WMCTRL_NUMBER} )
+    exit
+  fi
+done
---
+++ win/focus_vnyan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+WINDOW_CLASS="VNyan"
+
+COMMAND=""
+
+${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
