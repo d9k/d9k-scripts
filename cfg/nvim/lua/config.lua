a4f39ea3f83cb660a83b572be3fe9a92593d4490
d9k <d9k@ya.ru>
2025-10-20 14:29:00 +0500

    neovim config: add vim surround, text case plugins

--- cfg/nvim/lua/config.lua
+++ cfg/nvim/lua/config.lua
@@ -16,6 +16,31 @@ local lazy_config = require "configs.lazy"
 -- load plugins
 require("lazy").setup({
   {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      "ga", -- Default invocation prefix
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+    },
+    cmd = {
+      -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+      "Subs",
+      "TextCaseOpenTelescope",
+      "TextCaseOpenTelescopeQuickChange",
+      "TextCaseOpenTelescopeLSPChange",
+      "TextCaseStartReplacingCommand",
+    },
+    -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+    -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+    -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+    lazy = false,
+  },
+
+  {
     "NvChad/NvChad",
     lazy = false,
     branch = "v2.5",
@@ -23,6 +48,17 @@ require("lazy").setup({
     config = function()
       require "options"
     end,
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   },
 
   { import = "plugins" },
