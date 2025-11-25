2641be2be764c7c34f4734b8518912c3a247830d
d9k <d9k@ya.ru>
2025-11-25 21:25:46 +0500

    gpg-keyring-fix: SSH_AUTH_SOCK permanent socket path

--- gpg-keyring-fix.sh
+++ gpg-keyring-fix.sh
@@ -6,11 +6,28 @@
 
 OUT="$HOME/.keyring-fix"
 
-sudo killall ssh-agent
-sudo killall gnome-keyring-daemon
-sudo killall pinentry
+(
+  set -x
+  sudo killall ssh-agent
+  sudo killall gnome-keyring-daemon
+  sudo killall pinentry
+)
 
-ssh-agent -s > "$OUT"
+sleep 1
+
+if [[ -n "$SSH_AUTH_SOCK" ]]; then
+  echo "Binding to permanent address \$SSH_AUTH_SOCK"
+  echo "to bind keyring to random socket run:"
+  echo "export SSH_AUTH_SOCK="
+  # unix_listener: cannot bind to path : No such file or directory
+  # ssh-agent -s -a "$SSH_AUTH_SOCK" > "$OUT"
+  SSH_AUTH_SOCK_DIR=$(dirname $SSH_AUTH_SOCK)
+  mkdir -p "$SSH_AUTH_SOCK_DIR"
+  chmod go-rwx "$SSH_AUTH_SOCK_DIR"
+  ( set -x; ssh-agent -s -a $SSH_AUTH_SOCK > "$OUT" )
+else
+  ( set -x; ssh-agent -s > "$OUT" )
+fi
 
 . "$OUT"
 
@@ -19,6 +36,7 @@ printf "export " >> "$OUT"
 
 #gnome-keyring-daemon --replace --components=pkcs11,secrets,ssh >> "$OUT"
 gnome-keyring-daemon --replace --components=pkcs11,secrets >> "$OUT"
+echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> "$OUT"
 
 echo "$OUT:"
 cat "$OUT"
