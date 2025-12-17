3ea2fa2ededefeba2729d9edbd91c5b8bf08c6a4
d9k <d9k@ya.ru>
2025-12-17 16:53:06 +0500

    find-biggest-files: add min size filter

--- find-biggest-files.sh
+++ find-biggest-files.sh
@@ -3,4 +3,12 @@
 # https://stackoverflow.com/questions/22598205/how-sort-find-result-by-file-sizes
 
 # find . -type f -printf '%s\t%p\n' | sort -n | cut -f2-
-find . -type f -printf '%k\t%p' | sort -n
+
+
+MIN_SIZE_KB="$1"
+
+if [[ -z "$MIN_SIZE_KB" ]]; then
+  MIN_SIZE_KB=512
+fi
+
+find . -type f -size +"$MIN_SIZE_KB"k -printf '%k	%p\n' | sort -n
