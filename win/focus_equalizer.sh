505352089e2ff356b45b23e69f54dc29fdef0ebb
d9k <d9k@ya.ru>
2025-12-23 20:50:03 +0500

    focus_equalizer, focus_sound: apply transparency

--- win/focus_equalizer.sh
+++ win/focus_equalizer.sh
@@ -3,7 +3,14 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 WINDOW_CLASS="pulseeffects.Pulseeffects"
+OPACITY=90
 
-COMMAND="gio launch /var/lib/flatpak/exports/share/applications/com.github.wwmm.pulseeffects.desktop"
+COMMAND="gio launch /var/lib/flatpak/exports/share/applications/com.github.wwmm.pulseeffects.desktop &"
 
 ${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
+
+if [ "$?" -eq "1" ]; then
+  echo "Command was executed"
+  sleep 4
+  $SCRIPT_DIR/set-opacity.sh $OPACITY "$WINDOW_CLASS"
+fi
--- win/focus_sound.sh
+++ win/focus_sound.sh
@@ -3,7 +3,14 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 WINDOW_CLASS="sound.cinnamon-setting"
+OPACITY=90
 
-COMMAND="cinnamon-settings sound"
+COMMAND="cinnamon-settings sound &"
 
 ${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
+
+if [ "$?" -eq "1" ]; then
+  echo "Command was executed"
+  sleep 1.5
+  $SCRIPT_DIR/set-opacity.sh $OPACITY "$WINDOW_CLASS"
+fi
