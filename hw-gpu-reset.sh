1d63ceb6e217fa9726c8f11cd4616bbd2c07ceb6
d9k <d9k@ya.ru>
2026-01-05 07:13:02 +0500

    hw-gpu-reset: check blocking processes

--- hw-gpu-reset.sh
+++ hw-gpu-reset.sh
@@ -3,6 +3,12 @@
 # CUDA support is broken after hibernate.
 # https://stackoverflow.com/questions/10871412/resetting-gpu-and-driver-after-cuda-error/57803619#57803619
 
+# sudo nvidia-smi --gpu-reset -i 0
+
+echo "Check if nvidia_uvm is in use (stop processes manually):"
+
+( set -x; sudo fuser -v /dev/nvidia-uvm )
+
 set -x 
 
 sudo rmmod nvidia_uvm
