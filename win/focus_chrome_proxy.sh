765a3eb8cf8aca072459a36061ec6e7073d07891
d9k <d9k@ya.ru>
2026-03-03 11:45:16 +0500

    chrome-proxy: update window name to 'Chrome Other Proxy'

--- win/focus_chrome_proxy.sh
+++ win/focus_chrome_proxy.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-WINDOW_CLASS="Chrome Proxy"
+WINDOW_CLASS="Chrome Other Proxy"
 
 COMMAND="$HOME/scripts/chrome-proxy"
 
