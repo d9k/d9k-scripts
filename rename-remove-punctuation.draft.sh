c24cc5569404c2504aa38c6b8d1cd09d5fd3c258
d9k <d9k@ya.ru>
2026-03-03 12:00:14 +0500

    [draft] rename-remove-punctuation

---
+++ rename-remove-punctuation.draft.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+function echo_and_eval() {
+  echo "> $@"
+  EVAL_RESULT=$(eval "$@")
+}
+
+CMD="rename-ask \"s/[:]+//g\""
+
+if [ "$#" -lt 1 ]; then
+  echo "> $CMD *"
+  eval "$CMD *"
+else
+  QUOTED_ARGS=$(printf ' %q' "$@")
+  CMD="$CMD $QUOTED_ARGS"
+  echo "> $CMD"
+  eval "$CMD"
+fi
+
