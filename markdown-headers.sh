5614acc068adae70594d8412b61c28d0ebede7ca
d9k <d9k@ya.ru>
2026-03-03 10:44:01 +0500

    markdown-headers: check vault

--- markdown-headers.sh
+++ markdown-headers.sh
@@ -6,9 +6,22 @@ function echoerr {
 
 FILE="$1"
 
-if [[ -z "$1" ]]; then
+VAULT_PATH="$HOME/tx"
+
+if [[ -z "$FILE" ]]; then
   echoerr "Error: markdown file path required"
   exit 1
 fi 
+
+if [[ ! -f "$FILE" ]]; then
+  FILE_PATH_IN_VAULT="$VAULT_PATH/$FILE"
+  if [[ -f "$FILE_PATH_IN_VAULT" ]]; then
+     echoerr "Found file in the text vault: \"$FILE_PATH_IN_VAULT\""  
+     FILE="$FILE_PATH_IN_VAULT"
+  else
+     echoerr "File not found"
+     exit 2
+  fi
+fi
 
 cat "$FILE" | grep -E "^#" | sed 's|^# |\n# |'
