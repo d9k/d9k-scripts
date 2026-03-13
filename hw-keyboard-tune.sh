4da04e4721860115c7785f07519ade45d329f3f0
d9k <d9k@ya.ru>
2026-03-13 14:00:10 +0500

    hw-keyboard-tune

---
+++ hw-keyboard-tune.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+"$SCRIPT_DIR/hw-keyboard-swap-caps-lock-esc.sh"
+"$SCRIPT_DIR/hw-keyboard-fix-repeat-rate.sh
