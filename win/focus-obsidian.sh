de7122e87a419c1e01dc4734a71b616034e9460b
d9k <d9k@ya.ru>
2026-03-17 07:23:01 +0500

    chrome, obsidian: offload from Nvidia memory to integrated GPU with NVIDIA Prime

--- win/focus-obsidian.sh
+++ win/focus-obsidian.sh
@@ -6,6 +6,6 @@ WINDOW_CLASS="obsidian.obsidian"
 
 #COMMAND="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=obsidian.sh --file-forwarding md.obsidian.Obsidian"
 # COMMAND="obsidian"
-COMMAND="obsidian obsidian://open/?vault=tx"
+COMMAND="__NV_PRIME_RENDER_OFFLOAD=1 obsidian obsidian://open/?vault=tx"
 
 bash -c "$SCRIPT_DIR/focus-window-by-class.sh \"$WINDOW_CLASS\" \"$COMMAND\""
