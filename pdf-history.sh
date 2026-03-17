8c1cb132cc837004dc80958774974bbd943010eb
d9k <d9k@ya.ru>
2026-03-17 07:26:38 +0500

    pdf-history: fix for case when zathura history file doesn't exist

--- pdf-history.sh
+++ pdf-history.sh
@@ -2,16 +2,24 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+ZATHURA_HISTORY_FILE_PATH="$HOME/.local/share/zathura/history"
+
 function echoerr {
   printf "%s\n" "$*" >&2;
 }
 
-# https://unix.stackexchange.com/questions/467524/open-file-from-history-in-zathura
-SELECTION=$(sh -c "cat ~/.local/share/zathura/history | grep -Po '\[\K[^\]]*' | grep -v '\=\$' | tac | dmenu -l 40")
+SELECTION=
 
-if [[ -z "$SELECTION" ]]; then
-  echoerr "Nothing selected! Exitting"
-  exit
+# https://unix.stackexchange.com/questions/467524/open-file-from-history-in-zathura
+if [[ ! -f "$ZATHURA_HISTORY_FILE_PATH" ]]; then
+  echoerr "Error: Zathura history file not found: $ZATHURA_HISTORY_FILE_PATH"
+else
+  SELECTION=$(sh -c "cat $ZATHURA_HISTORY_FILE_PATH | grep -Po '\[\K[^\]]*' | grep -v '\=\$' | tac | dmenu -l 40")
+
+  if [[ -z "$SELECTION" ]]; then
+    echoerr "Nothing selected! Exitting"
+    exit
+  fi
 fi
 
 # https://rodrigo.morales.pe/2025/02/18/compile-zathura-0-5-11-in-ubuntu-24-04-lts/
