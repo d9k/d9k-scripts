56e27997e454a23fde0f9976ccd203a5c01bfe5c
d9k <d9k@ya.ru>
2026-03-17 07:25:16 +0500

    hw-keyboard-tune: fix scripts names

--- hw-keyboard-tune.sh
+++ hw-keyboard-tune.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-"$SCRIPT_DIR/hw-keyboard-swap-caps-lock-esc.sh"
-"$SCRIPT_DIR/hw-keyboard-fix-repeat-rate.sh
+$SCRIPT_DIR/hw-keyboard-swap-caps-lock-esc.sh
+$SCRIPT_DIR/hw-keyboard-fix-repeat-rate.sh
