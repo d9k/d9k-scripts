ad9b7103f7f2cb80c18ae96e4f11efbdf8ef50b7
d9k <d9k@ya.ru>
2026-03-18 06:18:28 +0500

    html-entities-decode on Deno

--- html-entities-decode.ts
+++ html-entities-decode.ts
@@ -1,4 +1,16 @@
-#!env deno
+#!/bin/env deno
 
 import { Html5Entities } from "https://deno.land/x/html_entities@v1.0/mod.js";
-console.log(Html5Entities.decode('&lt;&gt;&quot;&amp;&copy;&reg;'));
+import { stdout } from 'node:process';
+
+const pipeInputLines = [];
+
+const decoder = new TextDecoder();
+for await (const chunk of Deno.stdin.readable) {
+  const text = decoder.decode(chunk);
+  pipeInputLines.push(text);
+}
+
+const pipeInputString = pipeInputLines.join("\n");
+// console.log(Html5Entities.decode(pipeInputString));
+stdout.write(Html5Entities.decode(pipeInputString));
