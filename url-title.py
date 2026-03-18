c62fb849d37dba3b9a99167f78641cc9ac21e133
d9k <d9k@ya.ru>
2026-03-18 05:31:54 +0500

    url-title.py fix with GLM 4.7 assist

--- url-title.py
+++ url-title.py
@@ -1,10 +1,14 @@
 #!env python3
+# https://github.com/impredicative/urltitle
 # pip install urltitle
-
 from urltitle import URLTitleReader
+import sys
+
+try: url = sys.argv[1]
+except IndexError: print("Error: URL argument is required", file=sys.stderr); sys.exit(100)
 
 reader = URLTitleReader(verify_ssl=True)
 
 # Titles for HTML content
-title = reader.title('https://www.cnn.com/2019/02/11/health/insect-decline-study-intl/index.html')
+title = reader.title(url)
 print(title)
