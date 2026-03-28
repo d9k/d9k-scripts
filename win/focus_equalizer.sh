ffa49f8341e938a4699694685c0cd8919de84550
d9k <d9k@ya.ru>
2026-03-29 02:50:35 +0500

    focus_equalizer.sh: opacity change retries

--- win/focus_equalizer.sh
+++ win/focus_equalizer.sh
@@ -11,6 +11,18 @@ ${SCRIPT_DIR}/focus-window-by-class.sh "$WINDOW_CLASS" "$COMMAND"
 
 if [ "$?" -eq "1" ]; then
   echo "Command was executed"
-  sleep 4
-  $SCRIPT_DIR/set-opacity.sh $OPACITY "$WINDOW_CLASS"
+  SLEEP_BEFORE_RETRY_S=0.5
+  RETRY_NUMBER=0
+  MAX_RETRIES=20
+
+  while [[ "$RETRY_NUMBER" -lt "$MAX_RETRIES" ]]; do
+     $SCRIPT_DIR/set-opacity.sh $OPACITY "$WINDOW_CLASS"
+     if [ "$?" -eq "0" ]; then
+       exit
+     fi
+
+      ((RETRY_NUMBER+=1))
+      sleep "$SLEEP_BEFORE_RETRY_S"
+      echo "Window not found. Retry $RETRY_NUMBER"
+   done
 fi
