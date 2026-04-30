a3384541fe1980b1f6cc63497f88b41f3d280c74
d9k <d9k@ya.ru>
2026-04-30 06:01:10 +0500

    hw-gpu-hibernate-override-disable: show services statuses

--- hw-gpu-hibernate-override-disable.sh
+++ hw-gpu-hibernate-override-disable.sh
@@ -4,6 +4,10 @@
 
 set -x
 
+sudo systemctl status nvidia-suspend.service
+sudo systemctl status nvidia-hibernate.service
+sudo systemctl status nvidia-resume.service
+
 # https://forums.developer.nvidia.com/t/fixed-suspend-resume-issues-with-the-driver-version-470/187150/3
 sudo systemctl stop nvidia-suspend.service
 sudo systemctl stop nvidia-hibernate.service
