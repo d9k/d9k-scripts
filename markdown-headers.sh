49857baab8815c79aa1cb4553a0fa1b45b643c4c
d9k <d9k@ya.ru>
2026-06-15 00:51:55 +0500

    markdown-headers: support passing file path without quotes

--- markdown-headers.sh
+++ markdown-headers.sh
@@ -17,32 +17,7 @@ if [ "$#" -lt 1 ]; then
   help_exit 1
 fi
 
-# echo "$@"
-# echo
-# FILE="$1"
-# argv=( "$@" )
-FILE="${!#}"
-# argv=("${@:0:$#-1}")
-
-
-
 VAULT_PATH="$HOME/tx"
-
-if [[ -z "$FILE" ]]; then
-  echoerr "Error: markdown file path required"
-  exit 1
-fi 
-
-# Strip the last arg
-set -- "${@:1:$#-1}"
-# unset 'argv[-1]'
-# echo "${argv[@]}"
-# echo "$@"
-# echo
-# printf '%s\n' "$*"
-# echo
-# echo "FILE=$FILE"
-# exit
 
 GREP_ARGS=()
 
@@ -53,19 +28,42 @@ while [[ $# > 0 ]]; do
         -n|--line-number)
             SHOW_LINE_NUMBERS=1
             GREP_ARGS+=('--line-number')
-            shift # past argument
+            # shift # past argument
         ;;
         *)
           if [[ "${OPTION:0:1}" == "-" ]]; then
             echo -e "Error: unknown option \"$OPTION\" \n"
             help_exit
           else
-            break
+            break # while
           fi
         ;;
     esac
     shift # past argument or value
 done
+
+
+FILE="$@"
+# echo "$FILE"
+# argv=("${@:0:$#-1}")
+
+if [[ -z "$FILE" ]]; then
+  echoerr "Error: markdown file path required"
+  exit 1
+fi 
+
+
+# Strip the last arg
+# set -- "${@:1:$#-1}"
+# unset 'argv[-1]'
+# echo "${argv[@]}"
+# echo "$@"
+# echo
+# printf '%s\n' "$*"
+# echo
+# echo "FILE=$FILE"
+# exit
+
 
 if [[ ! -f "$FILE" ]]; then
   FILE_PATH_IN_VAULT="$VAULT_PATH/$FILE"
