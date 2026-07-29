734de2e367a94adda01c3e15992cb944c622952d
d9k <d9k@ya.ru>
2026-07-29 09:19:46 +0500

    net-wi-fi-hotspot-proxy-tunnel-{add,remove}: try fix 4

--- net-wi-fi-hotspot-proxy-tunnel-add.sh
+++ net-wi-fi-hotspot-proxy-tunnel-add.sh
@@ -3,9 +3,9 @@
 TUNNEL_ID="tun-hotspot-prx"
 TABLE_ID=199
 # TUNNEL_SUBNET="10.199.0.0"
-TUNNEL_SUBNET="10.199.0.0"
+TUNNEL_SUBNET="10.199.0.0/24"
 TUNNEL_IP="10.199.0.1"
-HOTSPOT_SUBNET="10.42.0.0"
+HOTSPOT_SUBNET="10.42.0.0/24"
 PROXY_PORT=10808
 SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 WI_FI_DEV="wlx503eaa78b4d2"
@@ -26,21 +26,29 @@ sudo ip addr add $TUNNEL_IP/24 dev "$TUNNEL_ID"
 sudo ip link set dev "$TUNNEL_ID" up
 
 sudo ip route add default via $TUNNEL_IP dev "$TUNNEL_ID" table $TABLE_ID
-sudo ip route add $HOTSPOT_SUBNET/24 dev "$WI_FI_DEV" table $TABLE_ID
-sudo ip route add $TUNNEL_SUBNET/24 dev $TUNNEL_ID table $TABLE_ID
+sudo ip route add $HOTSPOT_SUBNET dev "$WI_FI_DEV" table $TABLE_ID
+sudo ip route add $TUNNEL_SUBNET dev $TUNNEL_ID table $TABLE_ID
 
-sudo ip rule add from $HOTSPOT_SUBNET/24 table $TABLE_ID
+# У тебя ДВА правила маршрутизации — это конфликт! Оставь только fwmark
+# sudo ip rule add from $HOTSPOT_SUBNET/24 table $TABLE_ID
+
+# Удалить старое правило from (если есть)
+# sudo ip rule del from $HOTSPOT_SUBNET/24 table $TABLE_ID 2>/dev/null
 
 # Маркировка форвард-пакетов от хотспота
-sudo nft add rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1
+# sudo nft add rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1
+# sudo nft add rule ip filter FORWARD iifname "$WI_FI_DEV" ip saddr $HOTSPOT_SUBNET mark set 1 accept
+
+#  временно уберём ip saddr
+sudo nft insert rule ip filter FORWARD ip saddr $HOTSPOT_SUBNET mark set 1 accept
+# sudo nft insert rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1 accept
+
+# sudo nft insert rule ip filter FORWARD iifname "$WI_FI_DEV" ip saddr $HOTSPOT_SUBNET accept
 
 # Правило для маркированных пакетов
 sudo ip rule add fwmark 1 table $TABLE_ID
 
 # Проблема в том, что мы добавили mark set 1 в FORWARD, но до того, как пакет принимается в FORWARD, он должен пройти цепочку nm-sh-fw-wlx503eaa78b4d2, где разрешены только пакеты ct state related,established и от 10.42.0.0/24
-sudo nft insert rule ip filter FORWARD mark 1 accept
-
-# Удалить старое правило from (если есть)
-sudo ip rule del from $HOTSPOT_SUBNET/24 table $TABLE_ID 2>/dev/null
+# sudo nft insert rule ip filter FORWARD mark 1 accept
 
 sudo "$TUN_PATH" -device "$TUNNEL_ID" -proxy socks5://127.0.0.1:$PROXY_PORT
--- net-wi-fi-hotspot-proxy-tunnel-remove.sh
+++ net-wi-fi-hotspot-proxy-tunnel-remove.sh
@@ -3,12 +3,23 @@
 TABLE_ID=199
 TUNNEL_ID="tun-hotspot-prx"
 WI_FI_DEV="wlx503eaa78b4d2"
+HOTSPOT_SUBNET="10.42.0.0/24"
 
 set -x
 
-sudo nft delete rule ip filter FORWARD handle НОМЕР
-sudo nft delete rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1 
+sudo nft -a list chain ip filter FORWARD
+# sudo nft delete rule ip filter FORWARD handle НОМЕР
+# sudo nft delete rule ip filter FORWARD iifname "$WI_FI_DEV" ip saddr $HOTSPOT_SUBNET/24 accept
+# sudo nft delete rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1 
 sudo ip rule del fwmark 1 table $TABLE_ID
+# sudo nft delete rule ip filter FORWARD iifname "$WI_FI_DEV" ip saddr $HOTSPOT_SUBNET mark set 1 accept
+HANDLE=$(sudo nft -a list chain ip filter FORWARD 2>/dev/null | grep -oP 'mark set 0x00000001.*# handle \K\d+')
+if [ -n "$HANDLE" ]; then
+    sudo nft delete rule ip filter FORWARD handle "$HANDLE"
+fi
+# Нельзя удалять
+# sudo nft delete chain ip filter FORWARD
+sudo nft -a list chain ip filter FORWARD
 
 ip rule show table "$TABLE_ID"
 sudo ip route flush table "$TABLE_ID"
