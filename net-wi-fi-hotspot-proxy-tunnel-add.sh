5e25cdda95303c1aec0ed2be411b6d5f02ed5a36
d9k <d9k@ya.ru>
2026-07-29 08:29:59 +0500

    net-wi-fi-hotspot-proxy-tunnel-{add,remove}: try fix 3

--- net-wi-fi-hotspot-proxy-tunnel-add.sh
+++ net-wi-fi-hotspot-proxy-tunnel-add.sh
@@ -37,6 +37,9 @@ sudo nft add rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1
 # Правило для маркированных пакетов
 sudo ip rule add fwmark 1 table $TABLE_ID
 
+# Проблема в том, что мы добавили mark set 1 в FORWARD, но до того, как пакет принимается в FORWARD, он должен пройти цепочку nm-sh-fw-wlx503eaa78b4d2, где разрешены только пакеты ct state related,established и от 10.42.0.0/24
+sudo nft insert rule ip filter FORWARD mark 1 accept
+
 # Удалить старое правило from (если есть)
 sudo ip rule del from $HOTSPOT_SUBNET/24 table $TABLE_ID 2>/dev/null
 
--- net-wi-fi-hotspot-proxy-tunnel-remove.sh
+++ net-wi-fi-hotspot-proxy-tunnel-remove.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-TUNNEL_ID="tun-hotspot-prx"
 TABLE_ID=199
+TUNNEL_ID="tun-hotspot-prx"
+WI_FI_DEV="wlx503eaa78b4d2"
 
 set -x
+
+sudo nft delete rule ip filter FORWARD handle НОМЕР
+sudo nft delete rule ip filter FORWARD iifname "$WI_FI_DEV" mark set 1 
+sudo ip rule del fwmark 1 table $TABLE_ID
 
 ip rule show table "$TABLE_ID"
 sudo ip route flush table "$TABLE_ID"
