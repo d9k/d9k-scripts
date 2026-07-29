1218a6c219c169b939b35e53e38cec313e4ef0ef
d9k <d9k@ya.ru>
2026-07-29 07:57:57 +0500

    net-wi-fi-hotspot-proxy-tunnel-{add,remove}: try fix

--- net-wi-fi-hotspot-proxy-tunnel-add.sh
+++ net-wi-fi-hotspot-proxy-tunnel-add.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# TUNNEL_ID="tun-hotspot-proxy-199"
 TUNNEL_ID="tun-hotspot-prx"
 TABLE_ID=199
-TUNNEL_SUBNET="10.199.0.0"
+# TUNNEL_SUBNET="10.199.0.0"
+TUNNEL_SUBNET="10.199.0.1"
+TUNNEL_IP="10.199.0.1"
 HOTSPOT_SUBNET="10.42.0.0"
 PROXY_PORT=10808
 SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
@@ -12,8 +13,11 @@ sudo ip tuntap add mode tun dev $TUNNEL_ID
 sudo ip addr add $TUNNEL_SUBNET/24 dev "$TUNNEL_ID"
 sudo ip link set dev "$TUNNEL_ID" up
 
-sudo ip route add default via $TUNNEL_SUBNET dev "$TUNNEL_ID" table $TABLE_ID
+sudo ip route add default via $TUNNEL_IP dev "$TUNNEL_ID" table $TABLE_ID
 sudo ip rule add from $HOTSPOT_SUBNET/24 table $TABLE_ID
+
+# TODO в remove
+sudo ip route add /24 dev wlx503eaa78b4d2 table $TABLE_ID
 
 TUN_PATH=$(which tun2socks)
 
@@ -27,5 +31,3 @@ function cleanup_and_exit {
 trap cleanup_and_exit EXIT INT TERM
 
 sudo "$TUN_PATH" -device "$TUNNEL_ID" -proxy socks5://127.0.0.1:$PROXY_PORT
-
-
